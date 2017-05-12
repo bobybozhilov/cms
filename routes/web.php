b5525fd03e586a20f86698058dd292876f12c97e
bobybozhilov <bobybozhilov@abv.bg>
2017-05-12 23:18:20 +0300

    Chapter 5 Controllers completed

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return "HI about page";
-});
+//Route::get('/about', function () {
+//    return "HI about page";
+//});
+//
+//Route::get('/contact', function () {
+//    return "Hi i am contact";
+//});
+//
+//Route::get('/post/{id}/{name}', function($id, $name){
+//    return "$name this is number" . $id;
+//});
+//
+//Route::get('admin/posts/example',array('as'=>'admin.home' ,function(){
+//
+//    $url = route('admin.home');
+//
+//    return "this url is $url";
+//
+//}));
 
-Route::get('/contact', function () {
-    return "Hi i am contact";
-});
+//Route::get('post/{id}','PostsController@index');
 
-Route::get('/post/{id}/{name}', function($id, $name){
-    return "$name this is number" . $id;
-});
-
-Route::get('admin/posts/example',array('as'=>'admin.home' ,function(){
-
-    $url = route('admin.home');
-
-    return "this url is $url";
-
-}));
+Route::resource('posts', 'PostsController');
 
 Route::group(['middleware' => ['web']], function () {
 
